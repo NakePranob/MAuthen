6e3f53498816942412d1c410876336346e506db0
NakePranob <pranob.onphaeng@gmail.com>
2024-12-15 01:48:47 +0700

    add change password page

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -2,6 +2,7 @@
 import SignIn from '@/components/form/SignIn.vue';
 import { useAuthStore } from '@/stores/auth'
 import OTP from '@/components/form/OTP.vue';
+import ChangePassword from '@/components/form/ChangePassword.vue';
 import FormNoti from '@/components/form/FormNoti.vue';
 import { useRoute } from 'vue-router';
 
@@ -11,13 +12,52 @@ const queryParams = route.query;
 const auth = useAuthStore();
 const client_id = ref('Client Id');
 
-onMounted(() => {
+onMounted(async () => {
+    console.log("Page", auth.pageView)
     client_id.value = new URLSearchParams(window.location.search).get('client_id') || '';
     const fullUri = `?${Object.entries(queryParams)
         .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value as string)}`)
         .join('&')}`;
     console.log(fullUri);
     auth.setUri(fullUri);
+
+    const { data, error } = await useFetch<{
+        message: {
+            MinimumLength: number;  
+            RequireUppercase: boolean;
+            RequireLowercase: boolean;
+            RequireSymbols: boolean;
+            RequireNumbers: boolean;
+            TemporaryPasswordValidityDays: number;
+        };
+    }>('http://localhost:3002/api/v1/auth/passwordpolicy');
+
+    if (error.value) {
+        auth.setNotiSuccess({
+            isOpen: true,
+            state: 'error',
+            url: `http://localhost:3000/login${auth.uri}`,
+            message: 'noti-error-password-change-title',
+            description: 'noti-error-password-change-description',
+        });
+    }
+
+    if (data.value) {
+        console.log('Password Policy:', data.value.message);
+        auth.setPasswordPolicy({
+            MinimumLength: data.value.message.MinimumLength,
+            RequireUppercase: data.value.message.RequireUppercase,
+            RequireLowercase: data.value.message.RequireLowercase,
+            RequireSymbols: data.value.message.RequireSymbols,
+            RequireNumbers: data.value.message.RequireNumbers,
+        });
+        let height = 18 * 3;
+        if (data.value.message.RequireUppercase) height += 18;
+        if (data.value.message.RequireLowercase) height += 18;
+        if (data.value.message.RequireSymbols) height += 18;
+        if (data.value.message.RequireNumbers) height += 18;
+        auth.setPasswordPolicyHeight(height);
+    }
 });
 </script>
 
@@ -26,7 +66,8 @@ onMounted(() => {
         Required String parameter 'client_id' is not present
     </div>
     <div v-else class="max-w-[420px] w-full">
-        <SignIn v-if="auth.pageView != 'otp' && !auth.notiSuccess.isOpen" />
+        <SignIn v-if="auth.pageView != 'changePassword' && auth.pageView != 'otp' && !auth.notiSuccess.isOpen" />
+        <ChangePassword v-else-if="auth.pageView == 'changePassword'" />
         <OTP v-else-if="auth.pageView == 'otp'" />
         <FormNoti v-else-if="auth.notiSuccess.isOpen" />
     </div>
